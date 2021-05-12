ffb3196e3ef7a83a8c1b4391ae76302333733cb1
sushmithaB24 <82799245+sushmithaB24@users.noreply.github.com>
2021-05-12 23:12:32 +0530

    day 20 Task

---
+++ Tasks/Day 20/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApplication.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public string PostText { get; set; }
+
+        public string CommentText { get; set; }
+    }
+}
