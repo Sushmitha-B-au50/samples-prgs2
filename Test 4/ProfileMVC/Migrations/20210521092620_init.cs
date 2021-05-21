74c2c853275d9de12aa03ce22ea0829574d7cdc5
sushmithaB24 <82799245+sushmithaB24@users.noreply.github.com>
2021-05-21 20:51:53 +0530

    Test 4(day 26)

---
+++ Test 4/ProfileMVC/Migrations/20210521092620_init.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProfileMVC.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Qualification = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsEmployed = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoticePeriod = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CurrentCTC = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Profiles",
+                columns: new[] { "Id", "Age", "CurrentCTC", "IsEmployed", "Name", "NoticePeriod", "Qualification" },
+                values: new object[] { 1, 28, 45000f, "yes", "sita", "7/8/2022", "B.E CSE" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profiles");
+        }
+    }
+}
---
+++ Test 4/ProfileMVC/Models/ProfileContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProfileMVC.Models
+{
+    public class ProfileContext : DbContext
+    {
+        public ProfileContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Profile> Profiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Profile>().HasData(
+                new Profile() { Id=1,Name="sita",Age=28,Qualification="B.E CSE",IsEmployed="yes",NoticePeriod="7/8/2022",CurrentCTC= 45000 }
+                );
+          
+        }
+    }
+}
