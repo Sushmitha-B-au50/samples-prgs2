b2604e6037b6ccc58b55d83aa13c23373f248a0a
sushmithaB24 <82799245+sushmithaB24@users.noreply.github.com>
2021-06-16 16:06:30 +0530

    june16 task

---
+++ Tasks/DDList/Controllers/BookController.cs
@@ -0,0 +1,66 @@
+﻿using DDList.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DDList.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            BookModel bkm = new BookModel();
+            bkm.Books = PopulateBooks();
+            return View(bkm);
+        }
+        [HttpPost]
+        public ActionResult Index(BookModel bkm)
+        {
+            bkm.Books = PopulateBooks();
+            var selectedItem = bkm.Books.Find(p => p.Value == bkm.BookId.ToString());
+            if (selectedItem != null)
+            {
+                selectedItem.Selected = true;
+                ViewBag.Message = "Title: " + selectedItem.Text;
+                ViewBag.Message += "\\nQuantity: " + bkm.Quantity;
+            }
+
+            return View(bkm);
+        }
+
+        private static List<SelectListItem> PopulateBooks()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            string constr = "data source =DESKTOP-8CS1DM6; Integrated security= true;database = Books";
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string query = " SELECT Title, BookID FROM tbl_books";
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            items.Add(new SelectListItem
+                            {
+                                Text = sdr["Title"].ToString(),
+                                Value = sdr["BookID"].ToString()
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+
+            return items;
+        }
+
+
+    }
+}
